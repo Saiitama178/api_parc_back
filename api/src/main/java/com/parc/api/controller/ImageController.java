f4bb1664f74833de87961f71d7200789b28e9c3e
Lefebvre Tony <Tony59170@gmail.com>
2024-07-12 11:37:16 +0200

    push CreateImage in ImageController

--- api/src/main/java/com/parc/api/controller/ImageController.java
+++ api/src/main/java/com/parc/api/controller/ImageController.java
@@ -2,16 +2,16 @@ package com.parc.api.controller;
 
 import com.parc.api.model.dto.ImageDto;
 import com.parc.api.model.entity.Image;
+import com.parc.api.model.entity.Parc;
+import com.parc.api.model.entity.TypeImage;
 import com.parc.api.model.mapper.ImageMapper;
 import com.parc.api.repository.ImageRepository;
 import com.parc.api.repository.ParcRepository;
 import com.parc.api.repository.TypeImageRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -42,5 +42,17 @@ public class ImageController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping("/image/{idTypeImage}/{idParc}")
+    public ResponseEntity<ImageDto> createImage(@RequestBody ImageDto imageDto, @PathVariable("idTypeImage") int idTypeImage, @PathVariable("idParc") int idParc) throws Exception {
+        TypeImage typeImage = typeImageRepository.findById(idTypeImage)
+                .orElseThrow(()-> new Exception("erreur"));
+        Parc parc = parcRepository.findById(idParc)
+                .orElseThrow(()-> new Exception("erreur"));
+        Image image = ImageMapper.toEntity(imageDto, typeImage, parc);
+        Image savedImage = imageRepository.save(image);
+        ImageDto savedImageDto = ImageMapper.toDto(savedImage);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedImageDto);
     }
 }
