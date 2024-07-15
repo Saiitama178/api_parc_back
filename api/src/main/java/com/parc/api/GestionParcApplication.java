5d93338aaed736b4cc59552e3e7ada8c6566e86c
Saitama <badaoui.iliess@gmail.com>
2024-07-15 11:27:47 +0200

    MAJ

--- api/src/main/java/com/parc/api/GestionParcApplication.java
+++
@@ -1,9 +0,0 @@
-package com.parc.api;
-
-import org.springframework.boot.SpringApplication;
-
-public class GestionParcApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(GestionParcApplication.class, args);
-    }
-}
