a5925874a8f63d4ac09416d204a941f34e214eb6
olivier9 <duchatelle@gmail.com>
2024-08-20 16:33:35 +0200

    test PaysRepository

--- api/src/main/java/com/parc/api/repository/PaysRepository.java
+++ api/src/main/java/com/parc/api/repository/PaysRepository.java
@@ -3,5 +3,10 @@ package com.parc.api.repository;
 import com.parc.api.model.entity.Pays;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaysRepository extends JpaRepository<Pays, Integer> {
+
+//List<Pays>findByName(String nomPays);
 }
+
