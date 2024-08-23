355d1339896dbc9dab5eb60e3edb1895a58da0c8
olivier9 <duchatelle@gmail.com>
2024-08-23 11:35:33 +0200

    PaysService - getAllPays

---
+++ api/src/main/java/com/parc/api/service/PaysService.java
@@ -0,0 +1,26 @@
+package com.parc.api.service;
+
+import com.parc.api.model.dto.PaysDto;
+import com.parc.api.model.entity.Pays;
+import com.parc.api.model.mapper.PaysMapper;
+import com.parc.api.repository.PaysRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+
+public class PaysService {
+    private final PaysRepository paysRepository;
+    public List<PaysDto> getAllPays() {
+        List<Pays> paysList = paysRepository.findAll();
+        return paysList.stream()
+                .map(PaysMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+}
