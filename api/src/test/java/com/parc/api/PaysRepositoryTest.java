faa8f6677a24f3070eedb71c021ca799517ddc61
olivier9 <duchatelle@gmail.com>
2024-08-30 12:01:52 +0200

    initial

---
+++ api/src/test/java/com/parc/api/PaysRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.parc.api;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+public class PaysRepositoryTest {
+}
